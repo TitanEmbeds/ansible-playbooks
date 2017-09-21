0502693aa4cc256960ddb8f3579814e7475594e4
Jeremy "EndenDragon" Zhang <jeremyzhang5000@gmail.com>
2017-09-21 00:13:59 -0700

    Add recaptcha keys to config

--- roles/setup/files/config.webapp.example.py
+++ roles/setup/files/config.webapp.example.py
@@ -4,12 +4,16 @@ config = {
     'client-id': "Your app client id",
     'client-secret': "Your discord client secret",
     'bot-token': "Discord bot token",
-
+    
     # Rest API in https://developer.paypal.com/developer/applications
     'paypal-client-id': "Paypal client id",
     'paypal-client-secret': "Paypal client secret",
+    
+    # V2 reCAPTCHA from https://www.google.com/recaptcha/admin
+    'recaptcha-site-key': "reCAPTCHA v2 Site Key",
+    'recaptcha-secret-key': "reCAPTCHA v2 Secret Key",
 
-    'app-location': "/home/titan/Titan/webapp/",
+    'app-location': "/var/www/Titan/webapp/",
     'app-secret': "Type something random here, go wild.",
 
     'database-uri': "driver://username:password@host:port/database",
