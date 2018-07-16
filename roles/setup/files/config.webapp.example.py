ea5911ec27d73063f63d8c1733b848849790623b
Max Choi-Deyoung <34784905+maxchoid@users.noreply.github.com>
2018-07-16 12:58:55 -0400

    [X] make things more easier for the person that is installing the bot.

--- roles/setup/files/config.webapp.example.py
+++ roles/setup/files/config.webapp.example.py
@@ -1,25 +1,31 @@
 config = {
     # Create an app over here https://discordapp.com/developers/applications/me
     # and fill these fields out
-    'client-id': "Your app client id",
-    'client-secret': "Your discord client secret",
-    'bot-token': "Discord bot token",
+    'client-id': "Bot - Client ID",
+    'client-secret': "Bot - Secret",
+    'bot-token': "Bot - Token",
 
     # Rest API in https://developer.paypal.com/developer/applications
-    'paypal-client-id': "Paypal client id",
-    'paypal-client-secret': "Paypal client secret",
+    'paypal-client-id': "PayPal -  Client id",
+    'paypal-client-secret': "PayPal -  Client secret",
 
     # V2 reCAPTCHA from https://www.google.com/recaptcha/admin
-    'recaptcha-site-key': "reCAPTCHA v2 Site Key",
-    'recaptcha-secret-key': "reCAPTCHA v2 Secret Key",
+    # reCAPTCHA v1 is out of date and will not work with Titan Embeds, do not attempt to use reCAPTCHA v1 as it has been discontiuned by Google.
+    # https://i.imgur.com/Rm2NDnV.png
+    'recaptcha-site-key': "reCAPTCHA - v2 Site Key",
+    'recaptcha-secret-key': "reCAPTCHA - v2 Secret Key",
 
-     # Patreon from https://www.patreon.com/portal
+    # Patreon from https://www.patreon.com/portal
     'patreon-client-id': "Patreon client id",
     'patreon-client-secret': "Patreon client secret",
 
+    # The app-location is where the website will be located.
+    # In app-secret, you may type what ever you want to
     'app-location': "/var/www/Titan/webapp/",
-    'app-secret': "Type something random here, go wild.",
+    'app-secret': "Type random things in this field",
 
+    # Do not mess with database-uri and redis uri
+    # websockets-mode: recommended to use eventlet
     'database-uri': "driver://username:password@host:port/database",
     'redis-uri': "redis://",
     'websockets-mode': "LITTERALLY None or eventlet",
