d73a0388ed6ba8ebf90a573481452c8bbcfd0519
Max Choi-Deyoung <34784905+maxchoid@users.noreply.github.com>
2018-07-16 12:46:52 -0400

    clarification.

--- roles/setup/files/config.example.py
+++ roles/setup/files/config.example.py
@@ -1,11 +1,15 @@
+# The fields that indicate not to mess with are not to mess with unless you know what you are doing and getting yourself into.
+
 config = {
+    # Discord Bot Token
     'bot-token': "Bot - Token",
 
+    # Do not mess with!
     'database-uri': "driver://username:password@host:port/database",
-
+     # Do not mess with!
     'redis-uri': "redis://",
-
+     # Do not mess with
     'errorreporting-channelid': "",
-
+     # Do not mess with
     'logging-location': "/home/titan/Titan/discordbot/titanbot.log",
 }
