6e597beff63729e47df4dddfc1ec0ee623e8b6c3
Alex Howard <omnicons@gmail.com>
2018-07-16 20:56:32 -0400

    Reverting some of Max's stuff that was unwanted.

--- roles/setup/files/config.example.py
+++ roles/setup/files/config.example.py
@@ -1,8 +1,12 @@
 # The fields that indicate not to mess with are not to mess with unless you know what you are doing and getting yourself into.
 
 config = {
+<<<<<<< HEAD
     # Discord Bot Token
     'bot-token': "Bot - Token",
+=======
+    'bot-token': "Discord bot token",
+>>>>>>> parent of 022c944... added some clarification.
 
     # Do not mess with!
     'database-uri': "driver://username:password@host:port/database",
@@ -10,6 +14,10 @@ config = {
     'redis-uri': "redis://",
      # Do not mess with
     'errorreporting-channelid': "",
+<<<<<<< HEAD
      # Do not mess with
+=======
+    
+>>>>>>> parent of 022c944... added some clarification.
     'logging-location': "/home/titan/Titan/discordbot/titanbot.log",
 }
