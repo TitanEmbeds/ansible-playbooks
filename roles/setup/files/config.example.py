58f4c41c38126092ebd2cfa2ac8f92adb4088b57
Alex Howard <omnicons@gmail.com>
2018-07-17 21:32:00 -0400

    Fix revert error

--- roles/setup/files/config.example.py
+++ roles/setup/files/config.example.py
@@ -1,23 +1,17 @@
 # The fields that indicate not to mess with are not to mess with unless you know what you are doing and getting yourself into.
 
 config = {
-<<<<<<< HEAD
     # Discord Bot Token
     'bot-token': "Bot - Token",
-=======
     'bot-token': "Discord bot token",
->>>>>>> parent of 022c944... added some clarification.
 
-    # Do not mess with!
+    # Do not edit
     'database-uri': "driver://username:password@host:port/database",
-     # Do not mess with!
+     # Do not edit
     'redis-uri': "redis://",
-     # Do not mess with
+     # Do not edit
     'errorreporting-channelid': "",
-<<<<<<< HEAD
-     # Do not mess with
-=======
+     # Do not edit unless you need to
     
->>>>>>> parent of 022c944... added some clarification.
     'logging-location': "/home/titan/Titan/discordbot/titanbot.log",
 }
